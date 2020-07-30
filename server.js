9b204036a32a58c221062ef9f2eb191d8af0e7d4
Muhammed Kanlıdere <m.kanlidere01@gmail.com>
2020-07-30 21:04:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 const PORT = 8123;
 
 http.createServer(function (req, res) {
-  const message = "You are awesome dude!";
+  const message = "You are awesome dude! Stagig-push-test";
   console.log("INFO: "+message)
   res.write(PORT+ "" + message);
   res.end();
